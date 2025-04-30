594855ac783e1b30a11c8d6e4d33c1a903d391e1
pc-lrt-oaibox <tdeneux04@gmail.com>
2025-04-30 14:08:02 +0200

    v2

--- backend/mainbis.py
+++ backend/mainbis.py
@@ -5,11 +5,12 @@ app = FastAPI()
 
 PROMETHEUS_URL = "http://prometheus:9090"
 
-def query_prometheus(metric_name: str):
+def query_prometheus_container_status(container_name: str):
+    query = f'oai_container_status{{container="{container_name}"}}'
     try:
         response = requests.get(
             f"{PROMETHEUS_URL}/api/v1/query",
-            params={"query": metric_name}
+            params={"query": query}
         )
         data = response.json()
         if data["status"] != "success" or not data["data"]["result"]:
@@ -20,36 +21,36 @@ def query_prometheus(metric_name: str):
 
 @app.get("/core/upf/status")
 def get_upf_status():
-    return query_prometheus("oai_upf_status")
+    return query_prometheus_container_status("oai-upf")
 
 @app.get("/core/smf/status")
 def get_smf_status():
-    return query_prometheus("oai_smf_status")
+    return query_prometheus_container_status("oai-smf")
 
 @app.get("/core/amf/status")
 def get_amf_status():
-    return query_prometheus("oai_amf_status")
+    return query_prometheus_container_status("oai-amf")
 
 @app.get("/core/ausf/status")
 def get_ausf_status():
-    return query_prometheus("oai_ausf_status")
+    return query_prometheus_container_status("oai-ausf")
 
 @app.get("/core/udm/status")
 def get_udm_status():
-    return query_prometheus("oai_udm_status")
+    return query_prometheus_container_status("oai-udm")
 
 @app.get("/core/udr/status")
 def get_udr_status():
-    return query_prometheus("oai_udr_status")
+    return query_prometheus_container_status("oai-udr")
 
 @app.get("/core/nssf/status")
 def get_nssf_status():
-    return query_prometheus("oai_nssf_status")
+    return query_prometheus_container_status("oai-nssf")
 
 @app.get("/core/nrf/status")
 def get_nrf_status():
-    return query_prometheus("oai_nrf_status")
+    return query_prometheus_container_status("oai-nrf")
 
 @app.get("/core/ims/status")
 def get_ims_status():
-    return query_prometheus("oai_ims_status")
+    return query_prometheus_container_status("ims")
